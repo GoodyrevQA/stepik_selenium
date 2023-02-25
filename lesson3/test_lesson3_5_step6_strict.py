33a396713b25bc136a3e20f3e65231139681573d
Ivan <iegudyrev@gmail.com>
2023-02-25 21:17:47 +0300

    3.6 is started

--- lesson3/test_lesson3_5_step6_strict.py
+++ lesson3/test_lesson3_5_step6_strict.py
@@ -4,14 +4,22 @@ import pytest
 
 @pytest.mark.xfail(strict = True)
 def test_succeed():
+    print(1)
     assert True
 
+print(1.1)
 
-@pytest.mark.xfail
+
+@pytest.mark.xfail(reason='причина неизвестна')
 def test_not_succeed():
+    print(2)
     assert False
 
 
 @pytest.mark.skip
 def test_skipped():
+    print(3)
     assert False
+
+'''pytest -rx -v -s test_lesson3_5_step6_strict.py'''
+print(3.1)
